70053b248fb829924449460d8d6745c6e29f51b7
David Colarusso <colarusso@users.noreply.github.com>
2025-03-31 09:56:38 -0400

    first commit

---
+++ zine_text.js
@@ -0,0 +1,244 @@
+text = `<center style="margin-top:80px"><img src="https://lawrpg.org/images/d20.png" width="50%"></center><h1 style="width:100%;text-align:center;">Law RPG <br>Beginner's Handbook</h1>
+<center>
+By David Colarusso
+</center>
+
+<page_break>
+ 
+<p style="margin-top:312px">
+April 7, 2025 | #LITCon2025<br>
+</p>
+https://lawrpg.org<br>
+https://suffolklitlab.org
+
+<page_break>
+
+<h2 style="text-align:center;">Contents</h2>
+<p style="text-align:justify">
+Introduction . . . . . . . . . . . . . . . . . . . . . . . . . . . 1
+Ability Checks . . . . . . . . . . . . . . . . . . . . . . . . 2
+Roles . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 3
+&nbsp&nbspThe Judge . . . . . . . . . . . . . . . . . . . . . . . . . . . 3
+&nbsp&nbspFinders of Fact/Law . . . . . . . . . . . . . . . . . . 4
+&nbsp&nbspAttorneys . . . . . . . . . . . . . . . . . . . . . . . . . . 5
+Case Sheets . . . . . . . . . . . . . . . . . . . . . . . . . 5
+Sample Gameplay . . . . . . . . . . . . . . . . . . . . . . 9
+Interactive Helper . . . . . . . . . . . . . . . . . . . . . 14
+</p>
+<h2><a href="#wisdom" name="wisdom" class="anchor"></a>Introduction</h2>
+&nbsp;&nbsp;&nbsp;&nbsp;In role-playing games (RPGs) players assume the roles of characters in a simulated world. Not just anything goes, however. Outcomes are shaped by structure &amp; rules, the result of a player's actions interacting with an imagined reality. This handbook and the accompanying session at LITCon2025 will show you how to turn legal cases into simple tabletop RPGs by wrapping some rules around legal fact patterns and gathering a group of three or more players.
+
+<p class="page_number" style="margin-top:9px">1</p>
+<page_break>
+
+<h2 style="margin-top:10px">Ability Checks</h2>
+
+&nbsp;&nbsp;&nbsp;&nbsp;When a player takes an action in-game and the outcome is uncertain (e.g., whether an attorney's argument succeeds), RPGs provide users with a way to decide what happens. To keep it fair, these decisions aren't left up to one player, but to keep it believable, easy things need to be easy and hard things hard. The process of checking to see if a player is able to succeed at a task is called an <i>ability check</i>.
+
+<br>&nbsp;&nbsp;&nbsp;&nbsp;Here's how it works: (1) figure out how hard the task is—this is called the <i>difficulty class</i> (DC), and it will be a number between 1 and 20; (2) have the player taking the action role a 20-sided dice; (3) if their role is greater than or equal to the DC, they succeed! Other RPGs may add nuance by adding additional numbers to a roll, but we'll keep it simple and just use the number that comes up on the dice.
+
+<br>&nbsp&nbsp&nbsp&nbspThere are many ways to determine a DC. We'll talk about some options in a bit, but often you just "look it up." E.g., here's a table you can use.
+
+<p class="page_number">2</p>
+<page_break>
+
+<table><tr><td colspan="4">    <b>Typical Difficulty Classes</b>                                </td></tr>
+<tr><td>Very Easy &nbsp;&nbsp; </td><td>3  </td><td>| Hard                           </td><td>13</td></tr>
+<tr><td>Easy                   </td><td>7  </td><td>| Very Hard                      </td><td>17</td></tr>
+<tr><td>Medium                 </td><td>10 </td><td>| Nearly Impossible &nbsp;&nbsp; </td><td>20</td></tr>
+</table>
+
+<h2>Roles</h2>
+If we're going to be taking on roles, lets figure out what they are, but first, a word of caution. If you're not familiar with RPGs, things likely won't "click" until we get to the Sample Gameplay on page X. So, you only have to hold on a little longer.
+
+<h2>The Judge</h2>
+You may be wondering who gets to decide if a task is "hard," well the Judge of course. In other RPGs this role is often called the Game Master (GM). They are the nearly omniscient narrator of our world, and given our setting they also play the actual judge. That is, they'll explain to everyone what's going on, answering questions and giving instructions that are "above" the game, but they also act
+
+<p class="page_number">3</p>
+<page_break>
+
+like the judge, playing that part as they interact with the attorneys. In-game, however, they don't tend to make rulings. If there are only 3 players, or a ruling requires special knowledge not held by the <i>fractional judges</i> (explained below), they get to pick a DC. Otherwise, they should poll the <i>fractional judges</i> to determine the DC. I'll explain that in a bit.
+
+<br>&nbsp;&nbsp;&nbsp;&nbsp;The Judge shapes the gameplay, deciding where to focus. They can be a stickler for procedure or wave their hands, depending on their goals and comfort. They also get to turn up or turn down the detail. Maybe they'll make everyone speak just as they would in court, or suggest folks talk about themselves in the third-person, e.g., "Here's where I'd make an argument about X." If you're playing the judge, you'll get better the more you play. Think of yourself as a teacher guiding a classroom discussion, you're not trying to "win," you're trying to make it a good experience for your players/students.
+
+<h2>Finders of Fact/Law</h2>
+Hopefully, there are more than 3 people at 
+
+<p class="page_number">4</p>
+<page_break>
+
+your table. We want everyone to have a role even if they aren't an attorney or The Judge, here's how: they get to set the DC when called on by The Judge. Say the attorneys have just argued a point, how do we decide how hard success is? We take a poll. After an argument, the Judge asks the other non-attorney players to vote. The math's a bit bit simpler if the question is framed around the non-moving party, e.g., "How many of you think the non-moving party should succeed?" 
+
+<br>&nbsp;&nbsp;&nbsp;&nbsp;The Judge tallies those who think the motion should fail (same as the question above), divides it by the total number of voting players and multiplies this by 20 to get the DC. We have an app that can help. 
+
+<br>&nbsp;&nbsp;&nbsp;&nbsp;Then you have the movant role their dice. If their roll equals or beats the DC, they succeed!
+
+<br>&nbsp;&nbsp;&nbsp;&nbsp;These players can also act as traditional jurors or a panel of judges. If you're simulating a trial or an appellant argument, you might not need to set a lot of DCs. You might turn gameplay into the jury or judges' deliberation after the close of arguments. This makes for great conversations. 
+
+<p class="page_number">5</p>
+<page_break>
+
+<h2 style="margin-top:3px">Attorneys</h2>
+Attorneys do what you expect. It's their job to advocate for their side: research case law, make motions, examine witnesses, and persuade the finders of law and fact, but most importantly, they get to roll the dice!
+
+<h2>Case Sheets</h2>
+&nbsp;&nbsp;&nbsp;&nbsp;The case sheet is how the judge knows what to do. You can simulate a case at various points in its life cycle. Here we'll introduce you to a case sheet and explain how it's used. 
+
+<br>&nbsp;&nbsp;&nbsp;&nbsp;Functionally, the sheet acts as a proposed set of jury instructions (the rows in the elements column) and a witness list (subsequent columns), along with expected testimony lined up with its source and the element it aims to bolster or undermine. If you aim to simulate a case from the start, you begin with an empty table.
+
+<br>&nbsp;&nbsp;&nbsp;&nbsp;Each attorney is trying to fill in the sheet to build the best possible case for their client. Here's a simplified sample sheet.
+<p class="page_number">6</p>
+
+<page_break>
+
+<table cellpadding=5px cellspacing=0 style="transform: rotate(270deg);height:210px;width:350px;border:1px solid #555;margin-top:75px;margin-left:-68px">
+<tr><td style="border-bottom:1px solid #555;border-right:1px solid #555;"><b>Elements</b></td><td style="border-bottom:1px solid #555;text-align:center;"><b>Witness 1</b></td><td rowspan=5 style="border-left:1px solid #555;border-right:1px solid #555;text-align:center;">...</b></td></td><td style="text-align:center;border-bottom:1px solid #555;"><b>Witness <i>n</i></b></td></tr>
+
+<tr><td style="border-bottom:1px solid #555;border-right:1px solid #555;">
+D had a duty to P
+</td><td style="border-bottom:1px solid #555;">
+Store required to keep floors clear.
+</td><td style="border-bottom:1px solid #555;">
+Employees trained on maintaining cleanliness.
+</td></tr>
+
+
+<tr><td style="border-bottom:1px solid #555;border-right:1px solid #555;">
+D breached that duty
+</td><td style="border-bottom:1px solid #555;">  
+D's policy is for nightly cleaning, D was first in store
+</td><td style="border-bottom:1px solid #555;">
+Claims regular floor inspections were done.
+</td></tr>
+
+
+<tr><td style="border-bottom:1px solid #555;border-right:1px solid #555;">
+D breached that duty 
+</td><td style="border-bottom:1px solid #555;">  
+Saw Plaintiff slip on banana peel.
+</td><td style="border-bottom:1px solid #555;">
+Suggests Plaintiff was distracted by phone.
+</td>
+</tr>
+
+
+<tr><td style="border-right:1px solid #555;">
+P suffered actual harm
+</td><td> 
+Plaintiff appeared hurt; called ambulance.
+</td><td>
+Plaintiff seemed okay; walked without help.
+</td>
+</tr>
+
+</table>
+
+<page_break>
+
+&nbsp;&nbsp;&nbsp;&nbsp;Everything, is done in the shadow of the Case Sheet. I tend to break a simulation into three parts/sessions: (1) motion practice; (2) trial; and (3) oral arguments (appeals). In 1 we agree on the elements and witness list (first column and headers) and address things like motions to dismiss. In 2, we fill in the cells below each witness, and in 3 we treat the sheet as the factual record. 
+<br>&nbsp;&nbsp;&nbsp;&nbsp;Players prep for gameplay independently before we come together, with the attorneys do most of the hard work. I like to choose a case with an online docket containing at least a complaint and motion to dismiss. I tell folks to start there to figure out their elements and arguments. The docket may go beyond these, but I tell them our reality diverges here. I'm also a big fan of model jury instructions. 
+<h2>Sample Gameplay</h2>
+Having started with a blank Sheet, the first thing we do is fill in the elements. This may be done largely over email before meeting for arguments. You can then pick up where needed. For example:
+
+<p class="page_number">8</p>
+<page_break>
+
+<blockquote>
+<p style="margin-top:3px">JUDGE: I see we have agreement on all but one of the elements. Counselor, please explain to me why I should take your instruction on element three over the other side's.
+</p>
+<p>
+    ATTORNEY 1: [Makes an argument. If this is a low-fidelity simulation, the argument doesn't have to be polished, just cogent.]
+</p>
+<p>
+    JUDGE: Attorney Two?
+</p>
+<p>
+    ATTORNEY 2: [counter argument]
+</p>
+<p>
+    JUDGE: [speaking to the rest of the players] How many of you think Attorney Two had the better argument? 
+</p>
+<p>
+    [The rest of the players vote, determining a difficult class.]
+</p>
+<p>
+    JUDGE: The difficulty class is [X]. Roll. 
+</p>
+<p style="margin-bottom:0">
+    ATTORNEY 1: [Rolls a 20-sided dice. If their roll is larger than or equal to the difficulty class, the Judge will implement some version of their suggestion]...
+</p>
+</blockquote>
+
+<p class="page_number">9</p>
+<page_break>
+
+&nbsp;&nbsp;&nbsp;&nbsp;I cram a lot into a single motion session, playing loosely with sequence (e.g., I effectively start with jury instructions as a way to frame things). Judges may make rulings <i>sua sponte</i> if it serves their goals. Sometimes a case has too many elements, and I just dismiss them. I never dismiss everything, however, as I want there to be a trial. What I'm really trying to do is get the attorneys to tack down what they need to argue about and how they'll get evidence in (the elements and witness lists on our Sheet). Sometimes there's a Daubert challenge, and I make clear that players can always assume the federal rules of procedure & evidence. If there's a Q, they can step back form their character and ask for clarification. For D&amp;D players, think Intelligence Check. 
+
+<br>&nbsp;&nbsp;&nbsp;&nbsp;Remember, this is more than collaborative storytelling, it's collaborative sense making. The players are trying to understand a case, the motivations of parties, the strategies or actors. Once we have the elements and witnesses, it's time for trial.
+
+<br>&nbsp;&nbsp;&nbsp;&nbsp;I tend to skip any opening statements at trial. Rather, I simply move through each of 
+
+<p class="page_number" style="margin-top:5px">10</p>
+
+<page_break>
+
+the sheet's columns. Here the Judge has a role in setting difficulty classes. The simulation might go something like this:  
+
+<blockquote>
+<p>
+	JUDGE: We have here a witness for the plaintiff, and it says they would testify to facts A, B, and C on direct. I think that's a hard bargain. You need to roll 13 or above for that all to come in. Roll.  
+</p>
+<p>
+	ATTORNEY 1: [rolls 20-sided dice]
+</p>
+<p>
+	JUDGE: [provides a brief narration of the outcome based on the roll (e.g., if they rolled 13 or above they describe how all of that evidence came in, under 13 how it didn't all come in)] Attorney 2, what did you hope to accomplish on cross? 
+</p>
+<p>
+	ATTORNEY 2: I want to impeach them and get them to break down on the stand while screaming, "You can't handle the truth!"
+</p>
+<p>
+	JUDGE: Woah, alright, you have to roll a 20 for that...
+</p>
+</blockquote>
+
+At the close of trial I tend to let attorneys 
+<p class="page_number">11</p>
+
+<page_break>
+
+make a brief statement, but nothing prepared. The important thing is that the facts of the case are now part of the record (i.e., the Sheet is all filled out). After closings, we adjourn to the "Jury Room" and run a discussion among the jurors. It helps to appoint a foreperson, and they should have access to the Sheet. Normally, I maintain an online version of the sheet, using a cloud-based spreadsheet. 
+
+<br>&nbsp;&nbsp;&nbsp;&nbsp;Once the jury renders its verdict, it's time to identify something to appeal. Here I usually step in to identify something though I you could see what the losing attorney comes up with. I probably picked the case with an issue in mind, so it's mostly about structuring the challenge to focus on that issue and fit as an appeals. Sometimes this means you might have to retcon part of trial (i.e., editing the Sheet), but I think I only had to do this in 1 of 8 cases. 
+
+<br>&nbsp;&nbsp;&nbsp;&nbsp;Appeals lends itself to a high-fidelity simulation with no dice roles. I randomly draw three appeals judges from the players. Each attorney is then given ten minutes to speak, during which the judges may pepper them with questions and hypotheticals. 
+
+<p class="page_number">12</p>
+
+<page_break>
+
+Immediately following arguments we will break into a whole class discussion between the judges and their clerks (the remaining players). This discussion should be led by the judges. The judges will take their clerk's opinions into consideration when deciding on their vote. The attorneys must be quite during this discussion. 
+
+<br>&nbsp;&nbsp;&nbsp;&nbsp;The Judge will still help make things run smoothly and field questions about the world as they come up. For example, there may have been developments relevant to the real-world case and if players know about these, they may ask if they happened in-game too. The Judge can decided on such events. It may also be helpful for them to ask questions. 
+
+<br>&nbsp;&nbsp;&nbsp;&nbsp;Of course, you don't have to break things up like this. You could just do motions, or a trial. However, in my experience these sessions have lead to some really great class discussions. Also, it's a lot of fun.
+
+<br>&nbsp;&nbsp;&nbsp;&nbsp;After lunch, you'll have the chance to play a short game yourself. Don't worry; there should be folks to help out and you can always consult or interactive helper, link on the back of this booklet. 
+
+
+<p class="page_number">13</p>
+
+<page_break>
+
+<h2>Interactive Helper</h2>
+Visit https://lawrpg.org/litcon2025 to access this session's interactive helper. There you will find information for our case as well as guidance on how to run your simulation, included is a copy of this handbook. You can also get there with this QR code: 
+<center>
+<br>
+<img src="https://lawrpg.org/litcon2025/images/lawrpglitconqr.png" width="40%" border="0px"/>
+<br><br>
+If all you need is a dice (of any size), check out
+&nbsp;&nbsp;&nbsp;https://colarusso.github.io/dice/
+</center>
+
+<p class="page_number" style="margin-top:80px">14</p>
+`
